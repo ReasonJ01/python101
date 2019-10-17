d253009cc968e3ddee7a129205aa67067e09e28a
ReasonJ01 <ReasonJ01@pudseygrangefield.co.uk>
2019-10-17 13:01:15 +0100

    V0.2
    Added ask age.

--- main.py
+++ main.py
@@ -3,3 +3,10 @@ print("hello world")
 name = input("What's your name? ")
 
 print(f"Hello , {name}")
+
+age = int(input("What is your age? "))
+
+if age >= 18:
+	print("You are an adult.")
+else:
+	print("You are a child.")
